95dfc0d6eb5d319e52f5d06f8ea82cee2eced29b
PavelUd <paveludin4@gmail.com>
2025-05-24 13:55:51 +0500

    remove https 2

--- NetTalk/Web/Program.cs
+++ NetTalk/Web/Program.cs
@@ -27,13 +27,12 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+//    app.UseHsts();
 }
 
 //app.UseHttpsRedirection();
-//"https://localhost:7235;
-
 app.UseStaticFiles();
+//"https://localhost:7235;
 app.UseRouting();
 app.UseStaticFiles();
 
